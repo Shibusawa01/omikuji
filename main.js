855cd48f9419e3520310f929ee6dea3e4007fe6a
Shibusawa01 <68539805+Shibusawa01@users.noreply.github.com>
2020-07-22 12:39:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,21 @@
   // const results = ['大吉','大吉','大吉','大吉','凶'];
   // btn.textContent = results[Math.floor(Math.random()* results.length)];
   const n = Math.random();
-  if(n<0.05){
-    btn.textContent = '大吉';//5%
-  }else if(n<0.2){
-    btn.textContent = '中吉';//15%
+  if(n<0.2){
+    btn.textContent = 'カレーライス';
+    alert(btn.textContent);
+  }else if(n<0.4){
+    btn.textContent = '寿司';
+    alert(btn.textContent);
+  }else if(n<0.6){
+    btn.textContent = 'スパゲッティ';
+    alert(btn.textContent);
+  }else if(n<0.8){
+    btn.textContent = 'ハンバーガー';
+    alert(btn.textContent);
   }else{
-    btn.textContent = '凶';//80%
+    btn.textContent = 'ラーメン';
+    alert(btn.textContent);
   }
   });
 }
