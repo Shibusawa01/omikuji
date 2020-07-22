81ef8c82a2cce083675832069619314e9cf2fe2c
Touya Shibusawa <shibusawa@skcompany.tokyo>
2020-07-22 18:49:57 +0900

    remove none

--- main.js
+++ main.js
@@ -3,9 +3,6 @@
 {
   const btn = document.getElementById('btn');
   btn.addEventListener('click',()=>{
-  // const results = ['大吉','中吉','凶','末吉'];
-  // const results = ['大吉','大吉','大吉','大吉','凶'];
-  // btn.textContent = results[Math.floor(Math.random()* results.length)];
   const n = Math.random();
   if(n<0.2){
     btn.textContent = 'カレーライス';
