cfab828c7222bc7b35ac51919b31325adcdf52a8
Touya Shibusawa <shibusawa@skcompany.tokyo>
2020-07-22 17:46:28 +0900

    remove blank lines

--- main.js
+++ main.js
@@ -9,19 +9,14 @@
   const n = Math.random();
   if(n<0.2){
     btn.textContent = 'カレーライス';
-    
   }else if(n<0.4){
     btn.textContent = '寿司';
-    
   }else if(n<0.6){
     btn.textContent = 'スパゲッティ';
-   
   }else if(n<0.8){
     btn.textContent = 'ハンバーガー';
-    
   }else{
     btn.textContent = 'ラーメン';
-    
   }
   });
 }
