05bb67291f53ae1b1212a5233e7c1af3786bd62d
Touya Shibusawa <shibusawa@skcompany.tokyo>
2020-07-29 15:06:26 +0900

    コード整形

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
 
 {
   const btn = document.getElementById('btn');
-  btn.addEventListener('click',()=>{
-  const n = Math.random();
-  if(n<0.2){
-    btn.textContent = 'カレーライス';
-  }else if(n<0.4){
-    btn.textContent = '寿司';
-  }else if(n<0.6){
-    btn.textContent = 'スパゲッティ';
-  }else if(n<0.8){
-    btn.textContent = 'ハンバーガー';
-  }else{
-    btn.textContent = 'ラーメン';
-  }
+  btn.addEventListener('click', () => {
+    const n = Math.random();
+    if (n < 0.2) {
+      btn.textContent = 'カレーライス';
+    } else if (n < 0.4) {
+      btn.textContent = '寿司';
+    } else if (n < 0.6) {
+      btn.textContent = 'スパゲッティ';
+    } else if (n < 0.8) {
+      btn.textContent = 'ハンバーガー';
+    } else {
+      btn.textContent = 'ラーメン';
+    }
   });
 }
